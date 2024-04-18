3eba74d53feecf80b3aa99d72c7b8bcd14b183ff
b3h4re <lev.makarofff@gmail.com>
2024-04-18 13:55:25 +0300

    commented for server

--- mipt_first_year/room/models.py
+++ mipt_first_year/room/models.py
@@ -1,29 +1,29 @@
-from django.contrib.auth.models import User
-from django.db import models
-from config.settings import DEFAULT_USER_ID
-
-
-class Room(models.Model):
-    def get_last_message(self):
-        return self.messages.all().first()
-
-    @staticmethod
-    def get_default_user():
-        return User.objects.get(id=DEFAULT_USER_ID).username
-
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True)
-    room_student = models.ForeignKey(User, related_name="room_student", on_delete=models.DO_NOTHING,
-                                     default=get_default_user())
-    room_user = models.ForeignKey(User, related_name="room_user", on_delete=models.DO_NOTHING,
-                                  default=get_default_user())
-
-
-class Message(models.Model):
-    room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
-    content = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ('date_added',)
+# from django.contrib.auth.models import User
+# from django.db import models
+# from config.settings import DEFAULT_USER_ID
+#
+#
+# class Room(models.Model):
+#     def get_last_message(self):
+#         return self.messages.all().first()
+#
+#     @staticmethod
+#     def get_default_user():
+#         return User.objects.get(id=DEFAULT_USER_ID).username
+#
+#     name = models.CharField(max_length=256)
+#     slug = models.SlugField(unique=True)
+#     room_student = models.ForeignKey(User, related_name="room_student", on_delete=models.DO_NOTHING,
+#                                      default=get_default_user())
+#     room_user = models.ForeignKey(User, related_name="room_user", on_delete=models.DO_NOTHING,
+#                                   default=get_default_user())
+#
+#
+# class Message(models.Model):
+#     room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
+#     user = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
+#     content = models.TextField()
+#     date_added = models.DateTimeField(auto_now_add=True)
+#
+#     class Meta:
+#         ordering = ('date_added',)
