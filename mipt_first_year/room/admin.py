aa6a7de8c32e62a4c4a2b1676ecab84b9e026685
b3h4re <lev.makarofff@gmail.com>
2024-04-18 13:50:56 +0300

    commented for server

--- mipt_first_year/room/admin.py
+++ mipt_first_year/room/admin.py
@@ -1,6 +1,6 @@
-# from django.apps import apps
-# from django.contrib import admin
-#
-#
-# Room = apps.get_model('room', 'Room')
-# admin.site.register(Room)
+from django.apps import apps
+from django.contrib import admin
+
+
+Room = apps.get_model('room', 'Room')
+admin.site.register(Room)
--- mipt_first_year/student/models.py
+++ mipt_first_year/student/models.py
@@ -1,11 +1,11 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-#
-#
-# class Student(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     field_of_study = models.CharField(max_length=256)
-#     about = models.TextField(default='')
-#
-#     def __str__(self):
-#         return self.user.username
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    field_of_study = models.CharField(max_length=256)
+    about = models.TextField(default='')
+
+    def __str__(self):
+        return self.user.username
