069ca102303691dc33bbf01406e63f5f9553304a
root <root@kzdfhtmemw.local>
2024-05-17 08:18:35 +0000

    Fixed image load issue

--- mipt_first_year/users/models.py
+++ mipt_first_year/users/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -9,13 +11,38 @@ class Profile(models.Model):
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def save(self, *args, **kwargs):
+        wait_for_file(self.image.path)
         super().save(*args, **kwargs)
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
             output_size = (min(img.height, 300), min(img.width, 300))
             img.thumbnail(output_size)
+            img = img.convert("RGB")
             img.save(self.image.path)
 
     def __str__(self):
         return self.user.username
+
+
+def is_locked(filepath):
+    locked = None
+    file_object = None
+    if os.path.exists(filepath):
+        try:
+            buffer_size = 8
+            # Opening file in append mode and read the first 8 characters.
+            file_object = open(filepath, 'a', buffer_size)
+            if file_object:
+                locked = False
+        except IOError as message:
+            locked = True
+        finally:
+            if file_object:
+                file_object.close()
+    return locked
+
+def wait_for_file(filepath):
+    wait_time = 1
+    while is_locked(filepath):
+        time.sleep(wait_time)
